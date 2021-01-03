5f55ab296d879d800ce6fc273c8045edb9faf8f7
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-03 12:28:00 -0700

    (remove) unnesesary modules

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
 	entry: path.resolve(__dirname, "src/app.ts"),
-	mode: "development",
+	mode: "production",
 	target: "node",
 	module: {
 		rules: [
