7dca6938b67bcbb6ee8d8315270bd43738380d90
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-04 19:10:23 -0700

    fix(mongo uri): update node env variable for mongo uri

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -4,4 +4,4 @@ config();
 export const API_KEY: string = process.env.API_KEY;
 export const MODE: string = process.env.NODE_ENV;
 export const MONGO_URI: string =
-  MODE === 'prod' ? process.env.MONGO_URI : 'mongodb://localhost:27017/ecommerce';
+  MODE === 'production' ? process.env.MONGO_URI : 'mongodb://localhost:27017/ecommerce';
