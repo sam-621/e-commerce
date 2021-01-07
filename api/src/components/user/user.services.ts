f0c42cc1fdafb1f308f57a974e0de8f9d24484a8
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-07 13:28:09 -0700

    feat(services): add login service

--- api/src/components/user/user.services.ts
+++ api/src/components/user/user.services.ts
@@ -1,6 +1,8 @@
 import { IUser, IResponse } from './user.interface';
 import UserModel from './user.models';
+import { MODE, EXPIRES_IN, JWT_SECRET } from '../../config';
 import argon from 'argon2';
+import jwt from 'jsonwebtoken';
 
 class User {
   private readonly username: string;
@@ -36,13 +38,46 @@ class User {
       return { data: null, msg: 'INTERNAL SERVER ERROR', statusCode: 500, err: e };
     }
   }
+
+  public async login(): Promise<IResponse> {
+    try {
+      const user: IUser = await UserModel.findOne({ email: this.email }, 'password _id');
+
+      if (!user) {
+        return {
+          data: null,
+          msg: 'WRONG CREDENTIALS',
+          statusCode: 401,
+          err: 'No user founded',
+        };
+      }
+
+      const passwordsMatch = await argon.verify(user.password, this.password);
+
+      if (!passwordsMatch) {
+        return {
+          data: null,
+          msg: 'WRONG CREDENTIALS',
+          statusCode: 401,
+          err: 'Passwords dont match',
+        };
+      }
+
+      const payload = {
+        ID: user._id,
+      };
+
+      const token = jwt.sign(
+        payload,
+        JWT_SECRET,
+        MODE === 'production' ? { expiresIn: EXPIRES_IN } : null
+      );
+
+      return { data: token, msg: 'USER LOGGED', statusCode: 200, err: false };
+    } catch (e) {
+      return { data: null, msg: 'INTERNAL SERVER ERROR', statusCode: 500, err: e };
+    }
+  }
 }
 
 export { User };
-
-// user.save((err) => {
-//   if (err) {
-//     return { data: null, msg: 'EMAIL ALREADY TAKEN', statusCode: 400, err: err };
-//   }
-//   return { data: null, msg: 'USER REGISTERED', statusCode: 200, err: false };
-// });
--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -5,3 +5,5 @@ export const API_KEY: string = process.env.API_KEY;
 export const MODE: string = process.env.NODE_ENV;
 export const MONGO_URI: string =
   MODE === 'production' ? process.env.MONGO_URI : 'mongodb://localhost:27017/ecommerce';
+export const EXPIRES_IN: string = process.env.EXPIRES_IN || '3d';
+export const JWT_SECRET: string = process.env.JWT_SECRET || 'jwt_secret';
