d5b3241a06247b141c29527076720d08fb8b1f95
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-08 14:06:13 -0700

    feat(webpack): update webpack config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
 
 module.exports = {
-   entry: './src/index.tsx',
-   output: {
-     path: path.join(__dirname, 'dist/'),
-     publicPath: '/',
-     filename: 'bundle.js'
-  }
-}
+	entry: './src/index.tsx',
+	output: {
+		path: path.join(__dirname, 'dist/'),
+		publicPath: '/',
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js'],
+	},
+};
