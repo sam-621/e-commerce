08aa62c2c53c19b04e5c78cb2dbc93f4e122acc0
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-08 14:49:51 -0700

    feat(css): we can use css in our project

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: path.resolve(__dirname, 'src/index.tsx'),
@@ -19,11 +21,23 @@ module.exports = {
 					extensions: ['.ts', '.tsx', '.js'],
 				},
 			},
+			{
+				test: /\.css$/i,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					'css-loader',
+				],
+			},
 		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, 'public/index.html'),
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'styles.css',
 		}),
 	],
 	devServer: {
