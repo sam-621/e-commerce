2a371f954ba2fff20c92c2c4a9230538f65a701a
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-08 15:04:22 -0700

    fix(test): add api key

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 config();
 
-export const API_KEY: string = process.env.API_KEY;
+export const API_KEY: string = process.env.API_KEY || 'secret';
 export const MODE: string = process.env.NODE_ENV;
 export const MONGO_URI: string =
   MODE === 'production' ? process.env.MONGO_URI : 'mongodb://localhost:27017/ecommerce';
