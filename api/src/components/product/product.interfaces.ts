e38f3dc298b82970d944c32069c460ad87a3724b
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-11 17:22:58 -0700

    feat(product): add product model

--- api/src/components/product/product.interfaces.ts
+++ api/src/components/product/product.interfaces.ts
@@ -1,5 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
+import { Document, ObjectId } from 'mongoose';
 
 export interface IControllerWithToken {
   (req: Request, res: Response, next: NextFunction): Response;
+}
+
+export interface IProduct extends Document {
+  productName: string;
+  ProductDescription: string;
+  productPrice: number;
+  productImage: string;
+  buyer: ObjectId;
 }
