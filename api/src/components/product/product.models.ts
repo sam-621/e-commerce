298a57cb256a5288159f00323889381227d9cab4
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-11 17:37:15 -0700

    feat(products): add product model

--- api/src/components/product/product.models.ts
+++ api/src/components/product/product.models.ts
@@ -1,1 +1,14 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, Types } from 'mongoose';
+import { IProduct } from './product.interfaces';
+
+const ProductSchema: Schema = new Schema({
+  productName: { required: true, type: String },
+  ProductDescription: { required: true, type: String },
+  productPrice: { required: true, type: Number },
+  productImage: { required: true, type: String },
+  buyer: { required: true, type: Types.ObjectId },
+});
+
+const ProductModel = model<IProduct>('product', ProductSchema);
+
+export default ProductModel;
