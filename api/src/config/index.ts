a734e0de6dd3c8a7b3572e58f153fffca3b556b5
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-13 16:37:13 -0700

    fix(config): update config variables

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -2,9 +2,8 @@ import { config } from 'dotenv';
 config();
 
 export const MODE: string = process.env.NODE_ENV;
-export const API_KEY: string = MODE === 'dev' ? 'secret' : process.env.API_KEY || 'secret';
+export const API_KEY: string = MODE === 'production' ? process.env.API_KEY : 'secret';
 export const MONGO_URI: string =
   MODE === 'production' ? process.env.MONGO_URI : 'mongodb://localhost:27017/ecommerce';
 export const EXPIRES_IN: string = process.env.EXPIRES_IN || '3d';
-export const JWT_SECRET: string =
-  MODE === 'dev' ? 'jwt_secret' : process.env.JWT_SECRET || 'jwt_secret';
+export const JWT_SECRET: string = MODE === 'production' ? process.env.JWT_SECRET : 'jwt_secret';
