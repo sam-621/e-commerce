4ab15519292c775a6df0a9b06e770bc889f85764
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-14 17:44:57 -0700

    feat(molecules): add nav mobile component

--- client/src/components/molecules/NavMobile.tsx
+++ client/src/components/molecules/NavMobile.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import '../../styles/molecules/navMobile.css';
 
 import UserIcon from '../../img/usericon.svg';
+import { Link } from 'react-router-dom';
 
 const navMobile = () => {
   return (
-    <nav>
-      <div>
+    <nav className="NavMobile">
+      <Link to="/profile" className="NavMobile-userIcon">
         <img src={UserIcon} />
-      </div>
+      </Link>
     </nav>
   );
 };
