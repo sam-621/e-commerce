ff24373dbd19ba1a1939b91fe4e089d1d0c7ff93
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-15 18:56:39 -0700

    feat(register form): start to integrate front and back

--- client/src/components/containers/RegisterForm.tsx
+++ client/src/components/containers/RegisterForm.tsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, SyntheticEvent } from 'react';
 import '../../styles/containers/registerForm.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import { Input } from '../atoms/';
 
 const RegisterForm = () => {
+  const [username, setUsername] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+
+  function handleSubmit(e: SyntheticEvent): void {
+    e.preventDefault();
+
+    if (password.length < 6) {
+      alert('Password must have at least 6 characters');
+      return;
+    }
+  }
+
   return (
     <div className="RegisterForm-container">
       <div className="RegisterForm">
         <div className="RegisterForm-header">
           <h1>Create an account</h1>
         </div>
-        <form className="RegisterForm-form">
+        <form className="RegisterForm-form" onSubmit={handleSubmit}>
           <div className="RegisterForm-form-inputContainer">
+            <Input placeHolder="Username" setValue={setUsername} type="text" value={username} />
             <Input placeHolder="Email" setValue={setEmail} type="email" value={email} />
             <Input placeHolder="Password" setValue={setPassword} type="password" value={password} />
           </div>
