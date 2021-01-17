52f75f7055da9e0d138896015fe77dd250455fa0
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-16 17:46:34 -0700

    feat(login): 1st part of login

--- client/src/components/containers/LoginForm.tsx
+++ client/src/components/containers/LoginForm.tsx
@@ -1,19 +1,47 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, SyntheticEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import '../../styles/containers/registerForm.css';
+import axios from 'axios';
 
 import { Input } from '../atoms/';
+import { API_KEY, API_URI } from '../../config';
+import { HTTPException } from '../../utils/HttpException';
 
 const LoginForm = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const history = useHistory();
+
+  async function login(e: SyntheticEvent): Promise<void> {
+    e.preventDefault();
+
+    if (password.length < 6) {
+      alert('Password must have at least 6 characters');
+      return;
+    }
+
+    try {
+      const data = { email, password };
+      const res = await axios.post(`${API_URI}/login`, data, { headers: { api_key: API_KEY } });
+      console.log(res);
+
+      if (res.status === 200) {
+        history.push('/home');
+      }
+    } catch (e) {
+      const httpException = new HTTPException(e.message);
+      const message = httpException.getRegisterMessage();
+      alert(message);
+    }
+  }
+
   return (
     <div className="RegisterForm-container">
       <div className="RegisterForm">
         <div className="RegisterForm-header">
           <h1>Login</h1>
         </div>
-        <form className="RegisterForm-form">
+        <form className="RegisterForm-form" onSubmit={login}>
           <div className="RegisterForm-form-inputContainer">
             <Input placeHolder="Email" setValue={setEmail} type="email" value={email} />
             <Input placeHolder="Password" setValue={setPassword} type="password" value={password} />
