47299fd6890fb7d2d51c5203ac037fcd3b1e1fbf
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-18 11:43:49 -0700

    feat(product): add product html and add the add icon

---
+++ client/src/components/molecules/Product.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import AddIcon from '../../img/add.svg';
+
+const Product = ({ description, image, price, title }: IProductProps) => {
+  return (
+    <article className="Product">
+      <div className="Product-image">
+        <img src={image} alt={title} />
+      </div>
+      <div className="Product-info">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="Product-options">
+        <div className="Product-options-price">
+          <p>{price}</p>
+        </div>
+        <div className="Product-options-action">
+          <button>
+            <img src={AddIcon} alt="add to cart icon" />
+          </button>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+interface IProductProps {
+  image: string;
+  title: string;
+  description: string;
+  price: number;
+}
+
+export default Product;
