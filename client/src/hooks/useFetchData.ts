a6052c1ad8a36d31b40d28b1f116f90c20822ccf
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-18 12:56:16 -0700

    feat(hooks): add types to useFetchData

--- client/src/hooks/useFetchData.ts
+++ client/src/hooks/useFetchData.ts
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { API_KEY, API_URI } from '../config';
+import { IProducts } from './hooks.interfaces';
 
-function useFetchData(token: string) {
-  const [data, setData] = useState<Array<object>>([]);
+function useFetchData(token: string): { data: Array<IProducts>; loading: boolean } {
+  const [data, setData] = useState<Array<IProducts>>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const headers = { headers: { api_key: API_KEY, authorization: token } };
 
