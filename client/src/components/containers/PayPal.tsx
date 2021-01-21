8a428580a2d0128c42b1824e4a0007651850e9ce
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-21 16:24:44 -0700

    feat(payment): paypal integration works

--- client/src/components/containers/PayPal.tsx
+++ client/src/components/containers/PayPal.tsx
@@ -5,14 +5,35 @@ const PayPal = ({ amount }: IPayPalProps) => {
   const clientId =
     process.env.NODE_ENV === 'development' ? process.env.CLIENTID_DEV : process.env.CLIENTID_PROD;
 
+  function createOrder(data: any, actions: any) {
+    console.log(data, actions, 'create order');
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: amount,
+          },
+        },
+      ],
+    });
+  }
+
+  function onSuccess(details: any, data: any) {
+    console.log(details, data, 'onSuccess');
+
+    if (details.status === 'COMPLETED') {
+      alert('success');
+    }
+  }
+
   return (
     <section>
       <h1>Pay with PayPal</h1>
       <PayPalButton
         options={{ clientId: clientId, currency: 'MXN' }}
         amount={amount}
-        createOrder={() => console.log('order created')}
-        onApprove={() => console.log('Order approved')}
+        createOrder={createOrder}
+        onSuccess={onSuccess}
         style={{ layout: 'vertical', color: 'gold', shape: 'rect', label: 'pay' }}
       />
     </section>
