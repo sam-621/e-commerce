d3c00af486bb9b28356314256129153eff746500
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-06 08:46:30 -0700

    feat(api/user): update user schema

--- packages/api/src/components/user/user.models.ts
+++ packages/api/src/components/user/user.models.ts
@@ -5,8 +5,10 @@ const userSchema: Schema = new Schema({
   username: { type: String, required: false, default: '' },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  cart: { type: Array, required: false, default: [] },
+  productsBought: { type: Array, required: false, default: [] },
 });
 
-const UserModel = model<IUser>('user', userSchema);
+const UserModel = model<IUser>('users', userSchema);
 
 export default UserModel;
