3919709400cdbf61e4cda9a7408462d0e5bae072
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-05 22:44:07 -0700

    test(api): update products test

--- packages/api/__tests__/routes/products.test.ts
+++ packages/api/__tests__/routes/products.test.ts
@@ -1,7 +1,7 @@
 import App from '../../src/app';
 import req from 'supertest';
 import { API_KEY } from '../../src/config';
-import { dbClose, dbConnection, token } from '../utils/';
+import { dbClose, dbConnection, token, MockProduct } from '../utils/';
 const app = new App(3000).App;
 
 describe('Get products endpoint', () => {
@@ -25,44 +25,32 @@ describe('Buy a product endpoint', () => {
   afterAll(dbClose);
 
   test('Should response 401, NO TOKEN PROVIDED', async (done) => {
-    const res = await req(app).post('/products/buy').set('api_key', API_KEY).send({
-      productImage: 'https://arepa.s3.amazonaws.com/camiseta.png',
-      productName: 'Camiseta',
-      productPrice: 25,
-      productDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    });
+    const mockProduct = new MockProduct('image url', 'product', 25, 'description');
+    const res = await req(app).post('/products/buy').set('api_key', API_KEY).send(mockProduct);
 
     expect(res.status).toBe(401);
     done();
   });
 
   test('Should response 400, WRONG DATA SCHEMA', async (done) => {
+    const mockProduct = new MockProduct('', 'product', 25, 'description');
     const res = await req(app)
       .post('/products/buy')
       .set('api_key', API_KEY)
       .set('authorization', token)
-      .send({
-        productImage: '',
-        productName: 'Camiseta',
-        productPrice: 25,
-        productDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      });
+      .send(mockProduct);
 
     expect(res.status).toBe(400);
     done();
   });
 
   test('Should response 200, EVERYTHING OK', async (done) => {
+    const mockProduct = new MockProduct('image url', 'product', 25, 'description');
     const res = await req(app)
       .post('/products/buy')
       .set('api_key', API_KEY)
       .set('authorization', token)
-      .send({
-        productImage: 'https://arepa.s3.amazonaws.com/camiseta.png',
-        productName: 'Camiseta',
-        productPrice: 25,
-        productDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      });
+      .send(mockProduct);
 
     expect(res.status).toBe(200);
     done();
