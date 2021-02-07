48fe684c022910dc0a2b1c4dfb1c348dad73d2ae
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-07 12:38:03 -0700

    feat(frontend): add protected routes

--- packages/frontend/src/components/containers/Nav.tsx
+++ packages/frontend/src/components/containers/Nav.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import '../../styles/containers/nav.css';
 import { Link } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
 import UserIcon from '../../img/usericon.svg';
 
 const Nav = () => {
+  const cookie = new Cookies();
   return (
     <nav className="Nav">
       <div className="Nav-home">
         <Link to="/home">Shopy</Link>
       </div>
       <div>
-        <Link to="/profile" className="NavMobile-userIcon">
-          <img src={UserIcon} />
-        </Link>
+        {cookie.get('token') ? (
+          <Link to="/profile" className="NavMobile-userIcon">
+            <img src={UserIcon} />
+          </Link>
+        ) : null}
       </div>
     </nav>
   );
--- packages/frontend/src/components/pages/Home.tsx
+++ packages/frontend/src/components/pages/Home.tsx
@@ -1,20 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { Searcher } from '../molecules';
 import { Products, Nav } from '../containers';
-import { useAuth } from '../../hooks/useAuth';
 import Cookies from 'universal-cookie';
 import { Redirect } from 'react-router-dom';
 
 const Home = () => {
-  const [isAuth, setIsAuth] = useState<boolean>(false);
   const cookie = new Cookies();
-  useAuth(cookie.get('token')).then((result) => setIsAuth(result));
-  console.log(isAuth);
 
   return (
     <>
-      {isAuth ? (
+      {cookie.get('token') ? (
         <>
           <Nav />
           <Searcher />
