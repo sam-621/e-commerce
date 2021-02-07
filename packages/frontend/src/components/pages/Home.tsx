e938c347ce7ce03499416824209d78edcd74d92b
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-07 00:13:15 -0700

    feat(frontend/hooks): integrate useAuth hook

--- packages/frontend/src/components/pages/Home.tsx
+++ packages/frontend/src/components/pages/Home.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Searcher } from '../molecules';
 import { Products, Nav } from '../containers';
+import { useAuth } from '../../hooks/useAuth';
+import Cookies from 'universal-cookie';
+import { Redirect } from 'react-router-dom';
 
 const Home = () => {
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+  const cookie = new Cookies();
+  useAuth(cookie.get('token')).then((result) => setIsAuth(result));
+  console.log(isAuth);
+
   return (
     <>
-      <Nav />
-      <Searcher />
-      <Products />
+      {isAuth ? (
+        <>
+          <Nav />
+          <Searcher />
+          <Products />
+        </>
+      ) : (
+        <Redirect to="/login" />
+      )}
     </>
   );
 };
--- packages/frontend/src/hooks/useAuth.ts
+++ packages/frontend/src/hooks/useAuth.ts
@@ -1,24 +1,21 @@
 import axios from 'axios';
-import { useState } from 'react';
 import { API_KEY, API_URI } from '../config';
 
 export async function useAuth(token: string): Promise<boolean> {
-  const [isAuth, setIsAuth] = useState<boolean>(false);
+  if (!token) return false;
   const headers = { headers: { api_key: API_KEY, authorization: token } };
   try {
     const res = await axios.get(`${API_URI}/refresh`, headers);
 
     if (res.status === 401) {
-      setIsAuth(false);
+      return false;
     }
 
     if (res.status === 200) {
-      setIsAuth(true);
+      return true;
     }
   } catch (e) {
     console.log(e);
-    setIsAuth(false);
+    return false;
   }
-
-  return isAuth;
 }
