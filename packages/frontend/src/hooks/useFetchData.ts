83fea7e998c7860388a8288f0f9b77ae6449dc08
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-08 18:23:33 -0700

    feat(fronted): add axios instance

--- packages/frontend/src/hooks/useFetchData.ts
+++ packages/frontend/src/hooks/useFetchData.ts
@@ -1,19 +1,17 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { API_KEY, API_URI } from '../config';
 import { IProducts, IFetchedData } from './hooks.interfaces';
 import { HTTPException } from '../utils/HttpException';
+import AxiosInstance from '../utils/Axios';
 
-function useFetchData(token: string): IFetchedData {
+function useFetchData(): IFetchedData {
   const [data, setData] = useState<Array<IProducts>>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  const headers = { headers: { api_key: API_KEY, authorization: token } };
 
   async function fetchData() {
     try {
       setLoading(true);
 
-      const res = await axios.get(`${API_URI}/products`, headers);
+      const res = await AxiosInstance.get('/products');
       setData(res.data.data);
 
       setLoading(false);
