2a0d8dbbfee45229c8a8c180b5281b7ad98c48d2
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-09 16:14:41 -0700

    test(api): add test to addToCart endpoint

--- packages/api/__tests__/routes/products.test.ts
+++ packages/api/__tests__/routes/products.test.ts
@@ -56,3 +56,46 @@ describe('Buy a product endpoint', () => {
     done();
   });
 });
+
+describe('Add to cart endpoint', () => {
+  beforeAll(dbConnection);
+  afterAll(dbClose);
+
+  test('Should response 401, NO TOKEN PROVIDED', async (done) => {
+    const mockProduct = new MockProduct('image url', 'product', 25, 'description');
+    const res = await req(app).put('/products/cart').set('api_key', API_KEY).send(mockProduct);
+
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe('NO TOKEN PROVIDED');
+    done();
+  });
+
+  test('Should response 400, WRONG DATA SCHEMA', async (done) => {
+    const mockProduct = new MockProduct('', 'product', 25, 'description');
+    const res = await req(app)
+      .put('/products/cart')
+      .set('api_key', API_KEY)
+      .set('authorization', token)
+      .send(mockProduct);
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toBe('WRONG DATA SCHEMA');
+    done();
+  });
+
+  test('Should response 200, EVERYTHING OK', async (done) => {
+    const mockProduct = new MockProduct('image url', 'product', 25, 'description');
+    const mockUser = new MockUser('admin', 'admin@gmail.com', '123456');
+    const registerRes = await req(app).post('/register').set('api_key', API_KEY).send(mockUser);
+
+    const res = await req(app)
+      .put('/products/cart')
+      .set('api_key', API_KEY)
+      .set('authorization', registerRes.body.data)
+      .send(mockProduct);
+
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe('PRODUCT ADDED TO CART');
+    done();
+  });
+});
