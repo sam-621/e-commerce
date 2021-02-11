fad0cab0ff1c9b05d286a1e709078b547367e421
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-11 10:31:12 -0700

    feat(frontend/containers): add cartcard component

---
+++ packages/frontend/src/components/containers/CartCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const CartCard = ({ description, img, price, title }: ICartCard) => {
+  return (
+    <article>
+      <div>
+        <img src={img} alt={title} />
+      </div>
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+        <div>
+          <p>{price}</p>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+interface ICartCard {
+  img: string;
+  title: string;
+  description: string;
+  price: number;
+}
