289e1d43e0ca06da9f2e66cacaa171df1d864c7e
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-14 10:34:15 -0700

    feat(frontend): add remove all and buy al componentes

--- packages/frontend/src/components/containers/CartContent.tsx
+++ packages/frontend/src/components/containers/CartContent.tsx
@@ -4,6 +4,7 @@ import { ICtxReturns } from '../../context/interfaces';
 
 import { CartCard } from '../molecules/';
 import CartContext from '../../context/cart';
+import { Link } from 'react-router-dom';
 
 const CartContent = () => {
   const { productsCart } = useContext(CartContext) as ICtxReturns;
@@ -14,6 +15,10 @@ const CartContent = () => {
         {productsCart.map((prod) => {
           return <CartCard key={prod._id} img={prod.image} name={prod.name} price={prod.price} />;
         })}
+      </div>
+      <div className="CartContent-actions">
+        <Link to="#">Buy all</Link>
+        <button>Remove all</button>
       </div>
     </main>
   );
