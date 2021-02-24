8fd25fb5db36a782772ae7e9dcd48e82ab3aae79
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-23 20:08:56 -0700

    feat(frontend/context): add reducers

---
+++ packages/frontend/src/context/cart/reducer.ts
@@ -0,0 +1,24 @@
+import { IProduct } from '../interfaces';
+import { ADD_TO_CART } from './actions';
+
+const initialState: IInitialState = {
+  cart: [],
+};
+
+function cartReducer(state: IInitialState = initialState, action: IAction) {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return state;
+    default:
+      return state;
+  }
+}
+
+interface IInitialState {
+  cart: IProduct[];
+}
+
+interface IAction {
+  type: string;
+  data: IProduct;
+}
