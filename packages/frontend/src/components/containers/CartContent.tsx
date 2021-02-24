a81165996ea3dbb92d408c3331c01be88085cc2c
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-24 15:53:17 -0700

    feat(frontend/molecules): integrate remove from cart dispatcher

--- packages/frontend/src/components/containers/CartContent.tsx
+++ packages/frontend/src/components/containers/CartContent.tsx
@@ -1,20 +1,15 @@
 import React, { useContext } from 'react';
 import '../../styles/containers/cartContent.css';
-import { ICtxReturns } from '../../context/interfaces';
 
 import { CartCard } from '../molecules/';
 import CartContext from '../../context/cart/cart';
 import { Link } from 'react-router-dom';
-import { Loader } from '../atoms';
 
 const CartContent = () => {
   const [state] = useContext(CartContext) as any[];
 
   return (
     <main className="CartContent">
-      {/* {fetching ? (
-        <Loader border="5px" width="30px" height="30px" />
-      ) : ( */}
       <>
         <div className="CartContent-products">
           {state.cart.map((prod: any) => {
@@ -25,7 +20,6 @@ const CartContent = () => {
                 name={prod.name}
                 price={prod.price}
                 _id={prod._id}
-                setProductsCart={state.cart}
               />
             );
           })}
