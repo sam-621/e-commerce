ade02b2339e73e4ee4eb9c01f939c5e10f845186
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-23 20:30:00 -0700

    v1 of reducer (get)

--- packages/frontend/src/context/cart/cart.tsx
+++ packages/frontend/src/context/cart/cart.tsx
@@ -1,42 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { IProduct } from '../interfaces';
 import Cookie from 'universal-cookie';
 import AxiosInstance from '../../utils/Axios';
 import { API_KEY } from '../../config';
+import { cartReducer, initialState } from './reducer';
 
 const CartContext = React.createContext({});
 
 export function CartContextProvider({ children }: any) {
-  const [productsCart, setProductsCart] = useState<IProduct[]>([]);
-  const [fetching, setFetching] = useState<boolean>(false);
-  const cookie = new Cookie();
+  // const [productsCart, setProductsCart] = useState<IProduct[]>([]);
+  // const [fetching, setFetching] = useState<boolean>(false);
+  // const cookie = new Cookie();
 
-  async function getCartProducts() {
-    setFetching(true);
-    const token: string | null = cookie.get('token');
-    const headers = { api_key: API_KEY, authorization: token };
+  // async function getCartProducts() {
+  //   setFetching(true);
+  //   const token: string | null = cookie.get('token');
+  //   const headers = { api_key: API_KEY, authorization: token };
 
-    if (!token) return;
+  //   if (!token) return;
 
-    try {
-      const res = await AxiosInstance.get('/cart/get', { headers });
+  //   try {
+  //     const res = await AxiosInstance.get('/cart/get', { headers });
 
-      setProductsCart(res.data.data.cart);
-      setFetching(false);
-    } catch (e) {
-      setFetching(false);
-    }
-  }
+  //     setProductsCart(res.data.data.cart);
+  //     setFetching(false);
+  //   } catch (e) {
+  //     setFetching(false);
+  //   }
+  // }
 
-  useEffect(() => {
-    getCartProducts();
-  }, []);
-
-  return (
-    <CartContext.Provider value={{ productsCart, setProductsCart, fetching }}>
-      {children}
-    </CartContext.Provider>
-  );
+  // useEffect(() => {
+  //   getCartProducts();
+  // }, []);
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+  return <CartContext.Provider value={[state, dispatch]}>{children}</CartContext.Provider>;
 }
 
 export default CartContext;
--- packages/frontend/src/context/cart/reducer.ts
+++ packages/frontend/src/context/cart/reducer.ts
@@ -1,15 +1,42 @@
 import { IAction, IInitialState, IProduct } from '../interfaces';
-import { ADD_TO_CART } from './actions';
+import React, { useEffect, useState } from 'react';
+import { ADD_TO_CART, GET_CART } from './actions';
+import Cookie from 'universal-cookie';
+import AxiosInstance from '../../utils/Axios';
+import { API_KEY } from '../../config';
+const cookie = new Cookie();
 
 const initialState: IInitialState = {
   cart: [],
 };
 
-function cartReducer(state: IInitialState = initialState, action: IAction) {
+function cartReducer(state: IInitialState = initialState, action: IAction): IInitialState {
   switch (action.type) {
     case ADD_TO_CART:
       return state;
+
+    case GET_CART:
+      const token: string | null = cookie.get('token');
+      const headers = { api_key: API_KEY, authorization: token };
+
+      if (!token) return state;
+
+      try {
+        AxiosInstance.get('/cart/get', { headers }).then((res) => {
+          state.cart = res.data.data.cart;
+          return state;
+        });
+        // setProductsCart(res.data.data.cart);
+        // setFetching(false);
+      } catch (e) {
+        console.log(e);
+        return state;
+
+        // setFetching(false);
+      }
     default:
       return state;
   }
 }
+
+export { cartReducer, initialState };
