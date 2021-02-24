cf96397dc7fd6c70605a0f6e5242808713e498b3
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-24 15:29:16 -0700

    feat(frontend/context): add removeFromCart action creator

--- packages/frontend/src/context/cart/actions.ts
+++ packages/frontend/src/context/cart/actions.ts
@@ -1,2 +1,3 @@
 export const ADD_TO_CART: string = 'ADD_TO_CART';
 export const GET_CART: string = 'GET_CART';
+export const REMOVE_FROM_CART: string = 'REMOVE_FROM_CART';
--- packages/frontend/src/context/cart/actionsCreator.ts
+++ packages/frontend/src/context/cart/actionsCreator.ts
@@ -1,5 +1,5 @@
 import { IAction, IProduct } from '../interfaces';
-import { ADD_TO_CART, GET_CART } from './actions';
+import { ADD_TO_CART, GET_CART, REMOVE_FROM_CART } from './actions';
 
 function addToCartAction(product: IProduct): IAction {
   return {
@@ -17,4 +17,11 @@ function getCartProducts(products: IProduct[] = []): IAction {
   };
 }
 
-export { addToCartAction, getCartProducts };
+function removeFromCart(productID: string) {
+  return {
+    type: REMOVE_FROM_CART,
+    data: productID,
+  };
+}
+
+export { addToCartAction, getCartProducts, removeFromCart };
