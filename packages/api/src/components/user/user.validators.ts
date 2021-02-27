d6acad3bebf44a8a516e559d3e070f261ef25ec0
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-26 19:29:29 -0700

    fix(api/user): update the max length of username, thats why i couldnt register me

--- packages/api/src/components/user/user.validators.ts
+++ packages/api/src/components/user/user.validators.ts
@@ -1,7 +1,7 @@
 import { check, ValidationChain } from 'express-validator';
 
 export const registerValidator: Array<ValidationChain> = [
-  check('username', 'Must be a valid username').isLength({ max: 15 }).isString(),
+  check('username', 'Must be a valid username').isLength({ max: 20 }).isString(),
   check('email', 'Must be a valid email').isEmail(),
   check('password', 'Must be a valid password').isLength({ min: 6 }).isString(),
 ];
