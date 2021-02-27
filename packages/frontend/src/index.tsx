260efdf605389f7c3b3957dd1c01646d13043942
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-26 19:32:13 -0700

    feat(frontend): add a conditional for hot module replacement

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,7 +1,9 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import App from './app'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
 
-module.hot.accept();
+if (process.env.NODE_ENV === 'development') {
+  module.hot.accept();
+}
