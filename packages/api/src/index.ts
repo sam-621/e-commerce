3a6feb10ed28ed1cf926ce5f9906805396e30f8e
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-26 20:31:44 -0700

    remove console.log

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,8 +3,6 @@ import connect from './db/connect';
 import { MONGO_URI, PORT } from './config';
 
 function main(): void {
-  console.log('MONGO URI');
-  console.log(MONGO_URI);
   const server = new Server(PORT);
   connect(MONGO_URI);
   server.startServer();
