69ed327d6b44c0c1dbdde93f3bdd638f3da7c600
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-02 19:52:53 -0700

    fix(frontend): when go to login or register page i got an error

--- packages/frontend/src/components/pages/Login.tsx
+++ packages/frontend/src/components/pages/Login.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
 import { LoginForm, Nav } from '../containers/';
 
 const Login = () => {
+  const cookie = new Cookies();
   return (
     <>
-      <Nav />
-      <LoginForm />
+      {cookie.get('token') ? (
+        <Redirect to="/home" />
+      ) : (
+        <>
+          <Nav />
+          <LoginForm />
+        </>
+      )}
     </>
   );
 };
--- packages/frontend/src/components/pages/Register.tsx
+++ packages/frontend/src/components/pages/Register.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
 import { RegisterForm, Nav } from '../containers';
 
 const Register = () => {
+  const cookie = new Cookies();
   return (
     <>
-      <Nav />
-      <RegisterForm />
+      {cookie.get('token') ? (
+        <Redirect to="/home" />
+      ) : (
+        <>
+          <Nav />
+          <RegisterForm />
+        </>
+      )}
     </>
   );
 };
