69a92ebb05140cb2768ce63ead7252ae9ced5e20
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-06 11:11:36 -0700

    feat(frontend): add protectd routes in login and register page

--- packages/frontend/src/components/pages/Login.tsx
+++ packages/frontend/src/components/pages/Login.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+import { useAuth } from '../../hooks';
+import { Loader } from '../atoms';
 
 import { LoginForm, Nav } from '../containers/';
 
 const Login = () => {
+  const cookie = new Cookies();
+  const { finished, isAuth, token } = useAuth(cookie.get('token'));
+
+  if (token) {
+    cookie.set('token', token);
+  }
+
+  if (!finished) {
+    return <Loader border="5px" width="30px" height="30px" />;
+  }
+
   return (
     <>
-      <Nav isAuth={false} />
-      <LoginForm />
+      {isAuth ? (
+        <Redirect to="/home" />
+      ) : (
+        <>
+          <Nav isAuth={isAuth} />
+          <LoginForm />
+        </>
+      )}
     </>
   );
 };
