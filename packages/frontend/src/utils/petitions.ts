70f880b735f81545f1a9d1c515a2e5649af73e88
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-09 21:15:30 -0700

    feat(frontend): add post petition

--- packages/frontend/src/utils/petitions.ts
+++ packages/frontend/src/utils/petitions.ts
@@ -5,6 +5,10 @@ export const get = async (url: string, config?: IConfig): Promise<AxiosResponse>
   return await AxiosInstance.get(url, config);
 };
 
+export const post = async (url: string, data: any, config?: IConfig): Promise<AxiosResponse> => {
+  return await AxiosInstance.post(url, data, config);
+};
+
 interface IConfig {
   headers: {
     authorization: string;
