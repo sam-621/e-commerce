f873a06979f4cd8816a65b8b47d8335a342f5aae
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-11 21:14:30 -0700

    feat(frontend): i made the layout of ListProducts component

--- packages/frontend/src/components/molecules/ListProducts.tsx
+++ packages/frontend/src/components/molecules/ListProducts.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ListProducts = () => {
+const ListProducts = ({ title, url }: IListProductsProps) => {
   return (
     <div>
-      <h1>:)</h1>
+      <div>
+        <h3>{title}</h3>
+        <Link to={url}>See all</Link>
+      </div>
+      <div>
+        <h1>Products</h1>
+      </div>
     </div>
   );
 };
 
 export default ListProducts;
+
+interface IListProductsProps {
+  title: string;
+  url: string;
+}
