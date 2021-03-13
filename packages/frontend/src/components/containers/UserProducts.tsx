3452f105969589ce8b20ea49173d13eaff49203e
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-13 15:09:26 -0700

    feat(frontend): update styles in UserProducts components

--- packages/frontend/src/components/containers/UserProducts.tsx
+++ packages/frontend/src/components/containers/UserProducts.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../../styles/containers/userProducts.css';
 import { IProduct } from '../../context/interfaces';
 import { ListProducts } from '../molecules';
 
 const UserProducts = ({ productsBought, cartProducts }: IUserProductsProps) => {
   return (
-    <section>
+    <section className="UserProducts">
       <ListProducts products={productsBought} title="Products bought" url="products" />
       <ListProducts products={cartProducts} title="Cart products" url="cart" />
     </section>
