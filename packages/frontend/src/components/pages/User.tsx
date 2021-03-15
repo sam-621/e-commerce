1e0818ed27e5088696ad7a72b24cae753a828ca0
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-15 00:42:50 -0600

    feat(frontend): remove comments in User component

--- packages/frontend/src/components/pages/User.tsx
+++ packages/frontend/src/components/pages/User.tsx
@@ -1,32 +1,11 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import Cookies from 'universal-cookie';
-import { useAuth } from '../../hooks';
-import { Loader } from '../atoms';
 import { Nav, UserForms } from '../containers/';
 
 const User = () => {
-  // const cookie = new Cookies();
-  // const { token, isAuth, finished } = useAuth(cookie.get('token'));
-
-  // if (token) {
-  //   cookie.set('token', token);
-  // }
-
-  // if (!finished) {
-  //   return <Loader border="5px" width="30px" height="30px" />;
-  // }
-
   return (
     <>
-      {/* {isAuth ? (
-        <> */}
       <Nav isAuth={true} />
       <UserForms />
-      {/* </>
-      ) : (
-        <Redirect to="/login" />
-      )} */}
     </>
   );
 };
