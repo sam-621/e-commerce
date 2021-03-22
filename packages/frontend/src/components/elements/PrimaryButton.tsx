6370a84b4c5fe136000f14ac441bb2a7254bf1be
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-21 22:07:03 -0600

    feat(frontend): add PrimaryButton component

---
+++ packages/frontend/src/components/elements/PrimaryButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PrimaryButton = ({ children, type = 'submit', onClick }: IPrimaryButtonProps) => {
+  return (
+    <button type={type} onClick={onClick} className="PrimaryButton">
+      {children}
+    </button>
+  );
+};
+
+interface IPrimaryButtonProps {
+  children: string;
+  type: 'submit' | 'reset' | 'button';
+  onClick: VoidFunction;
+}
+
+export default PrimaryButton;
