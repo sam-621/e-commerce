2e4deb842131f1b8f45e2fce7bc29d418983a300
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-04-01 18:55:56 -0600

    test(frontend/context): finished test in cart context

--- packages/frontend/__test__/context/cart.test.ts
+++ packages/frontend/__test__/context/cart.test.ts
@@ -4,14 +4,31 @@ import {
   removeFromCart,
 } from '../../src/context/cart/actionsCreator';
 import { cartReducer } from '../../src/context/cart/reducer';
+import { IInitialState } from '../../src/context/interfaces';
 import { mockProducts, mockProduct } from './mockData';
 
 describe('Test Cart context', () => {
   const initialLength = mockProducts.length - 1;
+  const initialState: IInitialState = { cart: mockProducts };
 
-  test('add to cart', () => {
-    const res = cartReducer({ cart: mockProducts }, addToCartAction(mockProduct));
+  test('Add to cart', () => {
+    const res = cartReducer(initialState, addToCartAction(mockProduct));
 
     expect(res.cart[initialLength + 1].frontID).toBe(mockProduct.frontID);
+  });
+
+  test('Get cart products', () => {
+    const res = cartReducer(initialState, getCartProducts(initialState.cart));
+
+    expect(res.cart.length - 1).toBe(initialLength);
+  });
+
+  test('remove cart product', () => {
+    const res = cartReducer(initialState, removeFromCart('1'));
+
+    const productDeleted = res.cart.find((prod) => prod._id === '1');
+
+    expect(productDeleted).toBe(undefined);
+    expect(res.cart.length - 1).toBe(initialLength - 1);
   });
 });
