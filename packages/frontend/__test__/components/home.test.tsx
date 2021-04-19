37e5fea8214f4b38af524b4396739fb7704433d4
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-04-18 19:16:06 -0600

    upercase to M

--- packages/frontend/__test__/components/home.test.tsx
+++ packages/frontend/__test__/components/home.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Home from '../../src/components/modules/Home/Home';
-import MockComponent from '../utils/mockComponent';
+import MockComponent from '../utils/MockComponent';
 
 describe('<Home />', () => {
   test('should render', () => {
