224ab2d0ca3d8f2c71a4f5a377aebea581938963
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-04-24 23:16:23 -0600

    test(frontend): mock hook useAuth and get petition

--- packages/frontend/__test__/components/home.test.tsx
+++ packages/frontend/__test__/components/home.test.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { prettyDOM, screen, waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Home from '../../src/components/modules/Home/Home';
 import { CartContextProvider } from '../../src/context/cart/cart';
 import mountComponent from '../utils/mountComponent';
-import { Nav, Products } from '../../src/components/elements';
 import { productsData } from '../../src/products';
-import useAuth from '../../src/hooks/useAuth';
-import axios from 'axios';
 
-// const mockUseAuth = jest.fn(() => {
-//   return { isAuth: true, finished: true, token: '' };
-// });
+jest.mock('../../src/hooks', () => ({
+  useAuth: jest.fn(() => ({ isAuth: true, finished: true, token: '' })),
+}));
 
-jest.mock('../../src/hooks/useAuth.ts', () =>
-  jest.fn(() => ({ isAuth: true, finished: true, token: '' }))
-);
+jest.mock('../../src/utils/petitions', () => ({
+  get: jest.fn(() => ({ data: { data: productsData } })),
+}));
 
 describe('<Home />', () => {
   test('Should add to card', async () => {
