be43d6076eeed35b2f2079dea8074ff5e4ad6ebb
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-03 23:26:04 -0600

    feat(api/db): add products

---
+++ packages/api/src/db/products.ts
@@ -0,0 +1,134 @@
+const products = {
+  guajolotas: [
+    {
+      id: 1,
+      product: 'guajolota',
+      flavor: 'verde',
+      description: 'Verde',
+      price: 25,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/guajolotas/verdex2.png',
+    },
+    {
+      id: 2,
+      product: 'guajolota',
+      flavor: 'mole',
+      description: 'Mole',
+      price: 25,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/guajolotas/molex2.png',
+    },
+    {
+      id: 3,
+      product: 'guajolota',
+      flavor: 'guayaba',
+      description: 'Guayaba',
+      price: 25,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/guajolotas/guayabax2.png',
+    },
+    {
+      id: 4,
+      product: 'guajolota',
+      flavor: 'pina',
+      description: 'Piña',
+      price: 25,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/guajolotas/pinax2.png',
+    },
+    {
+      id: 5,
+      product: 'guajolota',
+      flavor: 'pasas',
+      description: 'Pasas',
+      price: 25,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/guajolotas/pasasx2.png',
+    },
+  ],
+  bebidas: [
+    {
+      id: 1,
+      product: 'bebida',
+      flavor: 'champurrado',
+      description: 'Champurrado',
+      price: 12,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/bebidas/champurradox2.png',
+    },
+    {
+      id: 2,
+      product: 'bebida',
+      flavor: 'atole',
+      description: 'Atole de Arroz con Leche',
+      price: 12,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/bebidas/arrozx2.png',
+    },
+    {
+      id: 3,
+      product: 'bebida',
+      flavor: 'chocolate',
+      description: 'Chocolate Caliente',
+      price: 12,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/bebidas/chocolatex2.png',
+    },
+    {
+      id: 4,
+      product: 'bebida',
+      flavor: 'cafe',
+      description: 'Café Negro',
+      price: 12,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/bebidas/cafex2.png',
+    },
+  ],
+  tamales: [
+    {
+      id: 1,
+      product: 'tamal',
+      flavor: 'verde',
+      description: 'Tamal Verde',
+      price: 14,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/tamales/verdex2.png',
+    },
+    {
+      id: 2,
+      product: 'tamal',
+      flavor: 'mole',
+      description: 'Tamal de Mole',
+      price: 14,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/tamales/molex2.png',
+    },
+    {
+      id: 3,
+      product: 'tamal',
+      flavor: 'guayaba',
+      description: 'Tamal de Guayaba',
+      price: 14,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/tamales/guayabax2.png',
+    },
+    {
+      id: 4,
+      product: 'tamal',
+      flavor: 'pina',
+      description: 'Tamal de Piña',
+      price: 14,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/tamales/pinax2.png',
+    },
+    {
+      id: 5,
+      product: 'tamal',
+      flavor: 'pasas',
+      description: 'Tamal de Pasas',
+      price: 14,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/tamales/pasasx2.png',
+    },
+  ],
+};
