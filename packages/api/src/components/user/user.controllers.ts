683a3b6fba42d2dca43c4f706d1e452de41a72df
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-10 21:52:47 -0600

    feat(api/user): response helper integrated in user services

--- packages/api/src/components/user/user.controllers.ts
+++ packages/api/src/components/user/user.controllers.ts
@@ -2,33 +2,24 @@ import { IController } from '../../types/controllers';
 import { User } from './user.services';
 import { AuthServices } from '../auth/auth.services';
 import { statusCodes } from '../../config';
+import response from '../../helpers/response';
 
 const registerController: IController = async (req, res) => {
   const { data, message, error } = await User.register(req.body);
 
-  return res.status(error?.statusCode || statusCodes.CONTENT_CREATED).json({
-    data,
-    message,
-    error: { message: error.message },
-  });
+  return response(res, data, message, statusCodes.CONTENT_CREATED, error);
 };
 
 const loginController: IController = async (req, res) => {
   const { data, message, error } = await User.login(req.body.username, req.body.password);
 
-  return res
-    .status(error?.statusCode || statusCodes.OK)
-    .json({ data, message, error: { message: error.message } });
+  return response(res, data, message, statusCodes.OK, error);
 };
 
 const getUserData: IController = async (req, res) => {
   const { data, error, message } = await AuthServices.getUser(req.user.id);
 
-  return res.status(error?.statusCode || statusCodes.OK).json({
-    data,
-    message,
-    error: { message: error.message },
-  });
+  return response(res, data, message, statusCodes.OK, error);
 };
 
 const updateUserData: IController = async (req, res) => {
@@ -36,21 +27,13 @@ const updateUserData: IController = async (req, res) => {
 
   const { data, error, message } = await User.UpdateUserInfo(req.user.id, username, email);
 
-  return res.status(error.statusCode || statusCodes.OK).json({
-    data,
-    message,
-    error: { message: error.message },
-  });
+  return response(res, data, message, statusCodes.OK, error);
 };
 
 const refreshTokenController: IController = async (req, res) => {
   const { data, error, message } = AuthServices.refreshToken(req.user.id);
 
-  return res.status(error?.statusCode || statusCodes.OK).json({
-    data,
-    message,
-    error: { message: error.message },
-  });
+  return response(res, data, message, statusCodes.OK, error);
 };
 
 export { registerController, loginController, refreshTokenController, getUserData, updateUserData };
