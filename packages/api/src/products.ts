e7759962875600238ec0fe448c6acc0372a6c8e5
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-13 16:31:24 -0600

    feat(api): add products file

---
+++ packages/api/src/products.ts
@@ -0,0 +1,138 @@
+import { IAllProducts } from './types/products';
+
+const products: IAllProducts = {
+  guajolotas: [
+    {
+      id: 1,
+      name: 'guajolota',
+      flavor: 'verde',
+      description: 'Verde',
+      price: 25,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/guajolotas/verdex2.png',
+    },
+    {
+      id: 2,
+      name: 'guajolota',
+      flavor: 'mole',
+      description: 'Mole',
+      price: 25,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/guajolotas/molex2.png',
+    },
+    {
+      id: 3,
+      name: 'guajolota',
+      flavor: 'guayaba',
+      description: 'Guayaba',
+      price: 25,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/guajolotas/guayabax2.png',
+    },
+    {
+      id: 4,
+      name: 'guajolota',
+      flavor: 'pina',
+      description: 'Piña',
+      price: 25,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/guajolotas/pinax2.png',
+    },
+    {
+      id: 5,
+      name: 'guajolota',
+      flavor: 'pasas',
+      description: 'Pasas',
+      price: 25,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/guajolotas/pasasx2.png',
+    },
+  ],
+  bebidas: [
+    {
+      id: 1,
+      name: 'bebida',
+      flavor: 'champurrado',
+      description: 'Champurrado',
+      price: 12,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/bebidas/champurradox2.png',
+    },
+    {
+      id: 2,
+      name: 'bebida',
+      flavor: 'atole',
+      description: 'Atole de Arroz con Leche',
+      price: 12,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/bebidas/arrozx2.png',
+    },
+    {
+      id: 3,
+      name: 'bebida',
+      flavor: 'chocolate',
+      description: 'Chocolate Caliente',
+      price: 12,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/bebidas/chocolatex2.png',
+    },
+    {
+      id: 4,
+      name: 'bebida',
+      flavor: 'cafe',
+      description: 'Café Negro',
+      price: 12,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/bebidas/cafex2.png',
+    },
+  ],
+  tamales: [
+    {
+      id: 1,
+      name: 'tamal',
+      flavor: 'verde',
+      description: 'Tamal Verde',
+      price: 14,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/tamales/verdex2.png',
+    },
+    {
+      id: 2,
+      name: 'tamal',
+      flavor: 'mole',
+      description: 'Tamal de Mole',
+      price: 14,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/tamales/molex2.png',
+    },
+    {
+      id: 3,
+      name: 'tamal',
+      flavor: 'guayaba',
+      description: 'Tamal de Guayaba',
+      price: 14,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/tamales/guayabax2.png',
+    },
+    {
+      id: 4,
+      name: 'tamal',
+      flavor: 'pina',
+      description: 'Tamal de Piña',
+      price: 14,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/tamales/pinax2.png',
+    },
+    {
+      id: 5,
+      name: 'tamal',
+      flavor: 'pasas',
+      description: 'Tamal de Pasas',
+      price: 14,
+      image:
+        'https://juan-jose-mayorga-projects.s3.amazonaws.com/react-native/guappjolotas/tamales/pasasx2.png',
+    },
+  ],
+};
+
+export default products;
--- packages/api/src/types/products.ts
+++ packages/api/src/types/products.ts
@@ -1,6 +1,5 @@
 export interface IProduct {
   id: number;
-  frontId: number;
   name: String;
   flavor: String;
   description: String;
