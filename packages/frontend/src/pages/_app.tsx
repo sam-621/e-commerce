9bfef13f569deef76ddfc0a2a7a9525e73681191
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-15 21:09:18 -0600

    refactor(frontend/styles): move styles folder into src folder

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
-import '../../styles/index.css';
+import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
