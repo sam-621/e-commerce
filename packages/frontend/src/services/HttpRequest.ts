70e383b3e5aa357942504c975d0dbbe4e57f5800
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-15 21:47:41 -0600

    feat(frontend/types): add IHeaders interface

--- packages/frontend/src/services/HttpRequest.ts
+++ packages/frontend/src/services/HttpRequest.ts
@@ -1,8 +1,16 @@
+import { BASE_URL } from '../config/envVars';
+
 export default class HttpRequest {
   private port: number;
   private baseUrl: string;
+  private endpoint: string;
+  private headers: object;
   constructor() {
     this.port = 3000;
-    this.baseUrl = process.env.BASE_URL;
+    this.baseUrl = BASE_URL;
+  }
+
+  public configEnpoint(endpoint: string) {
+    this.endpoint = endpoint;
   }
 }
---
+++ packages/frontend/src/types/services.ts
@@ -0,0 +1,5 @@
+export interface IHeaders {
+  api_key: string;
+  authorization?: string;
+  'Content-Type': 'application/json';
+}
