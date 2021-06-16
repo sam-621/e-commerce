911c7db53c3a4eba3c4df2d8d3e3080ac14aab76
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-15 21:35:20 -0600

    ci(frontend): update next.config file

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,9 +1,16 @@
+const Dotenv = require('dotenv-webpack');
+const withPlugins = require('next-compose-plugins');
+
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
 const nextConfig = {
   future: {
     webpack5: true,
+  },
+  webpack: (config) => {
+    config.plugins.push(new Dotenv({ systemvars: true }));
+    return config;
   },
 };
 
