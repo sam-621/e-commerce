619bca4fe4aca293f5ccf5c910fecf8b5147b184
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-17 22:13:58 -0600

    Se creo el componente Title

---
+++ packages/frontend/src/components/atoms/Title.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router';
+
+const Title = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <h1 role="heading"></h1>
+    </>
+  );
+};
+
+export default Title;
