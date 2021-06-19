293fab17c2ce7610fd91771f86ff3c0ccffe42a2
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-18 18:54:59 -0600

    envVars added

--- packages/api/src/config/envVars.ts
+++ packages/api/src/config/envVars.ts
@@ -3,10 +3,10 @@ config();
 
 const envVars = {
   MODE: process.env.NODE_ENV,
-  API_KEY: process.env.API_KEY,
-  MONGO_URI: process.env.MONGO_URI,
-  EXPIRES_IN: process.env.EXPIRES_IN,
-  JWT_SECRET: process.env.JWT_SECRET,
+  API_KEY: process.env.API_KEY || 'api_secret',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce',
+  EXPIRES_IN: process.env.EXPIRES_IN || '3h',
+  JWT_SECRET: process.env.JWT_SECRET || 'secret',
   PORT: process.env.PORT as unknown as number,
 };
 
