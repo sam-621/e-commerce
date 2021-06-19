78aab0673af0fdda196d8eb50f03f225b2786726
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-18 20:20:02 -0600

    TopMobileNav component updated

--- packages/frontend/src/components/containers/TopMobileNav.tsx
+++ packages/frontend/src/components/containers/TopMobileNav.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 import Title from '../atoms/Title';
+import CartButton from '../atoms/CartButton';
+import { useSelector } from 'react-redux';
+import { IReduxState } from '../../types/redux';
+import PrimaryButton from '../atoms/PrimaryButton';
 
 const TopMobileNav = () => {
+  const isLogged: boolean = useSelector<IReduxState>((state) => state.user.isLogged) as boolean;
   return (
     <nav>
       <div>
@@ -10,7 +15,7 @@ const TopMobileNav = () => {
       <div>
         <Title />
       </div>
-      <div></div>
+      {isLogged ? <CartButton /> : <PrimaryButton text="Register" type="button" />}
     </nav>
   );
 };
