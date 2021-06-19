2c6fe2a00ec8acda7103c323e581fbfe8a949133
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-18 19:47:00 -0600

    store created

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 import '../styles/index.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
--- packages/frontend/src/redux/store.ts
+++ packages/frontend/src/redux/store.ts
@@ -1,1 +1,28 @@
-export {};
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import { MODE } from '../config/envVars';
+import cart from './reducers/cart';
+
+const ignoredLogs: string[] = [];
+
+const logger = createLogger({
+  predicate: (_, action) => {
+    if (typeof window === 'undefined') {
+      return false;
+    }
+    return !ignoredLogs.includes(action.type);
+  },
+  level: 'info',
+  collapsed: () => true,
+});
+
+const store = createStore(
+  combineReducers({
+    cart,
+  }),
+  {},
+  MODE === 'DEV' ? applyMiddleware(thunk, logger) : applyMiddleware(thunk)
+);
+
+export default store;
