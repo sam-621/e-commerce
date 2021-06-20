f2accdfa30e0fae6f2bcbdd866e745a35f1cf174
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-20 13:58:04 -0600

    products home styles added

--- packages/frontend/src/components/templates/ProductsCarousel.tsx
+++ packages/frontend/src/components/templates/ProductsCarousel.tsx
@@ -1,11 +1,21 @@
 import { FC } from 'react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, EffectCoverflow } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { IProduct } from '../../types/products';
 import Product from '../molecules/Product';
 
+SwiperCore.use([Navigation, Scrollbar, Pagination, EffectCoverflow]);
+
 const ProductsCarousel: FC<IProductsCarouselProps> = ({ products }) => {
   return (
-    <Swiper tag="section" slidesPerView={2} spaceBetween={15} style={{ padding: '0 10px' }}>
+    <Swiper
+      tag="section"
+      slidesPerView={2}
+      spaceBetween={15}
+      style={{ padding: '0 10px' }}
+      pagination={{ dynamicBullets: true }}
+      lazy={false}
+    >
       {products?.map((item) => (
         <SwiperSlide>
           <Product
