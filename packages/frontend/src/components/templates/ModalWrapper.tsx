6b425eda327c0f3c0dd416104e9da9775e108522
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-20 20:04:09 -0600

    add closeModal function

--- packages/frontend/src/components/templates/ModalWrapper.tsx
+++ packages/frontend/src/components/templates/ModalWrapper.tsx
@@ -3,13 +3,15 @@ import { FC, useState } from 'react';
 const ModalWrapper: FC<IModalWrapperProps> = ({ children, isOpenProp }) => {
   const [isOpen, setIsOpen] = useState(isOpenProp);
 
+  const closeModal = () => setIsOpen(false);
+
   if (!isOpen) return null;
 
   return (
-    <div className="ModalWrapper">
+    <div className="ModalWrapper" onClick={closeModal}>
       <div className="ModalContent">
         <div className="ModalContent-close">
-          <button onClick={() => setIsOpen(false)}>X</button>
+          <button onClick={closeModal}>X</button>
         </div>
         {children}
       </div>
