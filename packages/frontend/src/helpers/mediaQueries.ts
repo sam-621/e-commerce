fff6453a9623788dbcfeba20f1450f32d1992586
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-25 21:05:37 -0600

    Media queries helper added

---
+++ packages/frontend/src/helpers/mediaQueries.ts
@@ -0,0 +1,8 @@
+export const mediaFromLg = '(min-width: 1024px)';
+export const mediaToLg = '(max-width: 1023.9px)';
+export const mediaFromM = '(min-width: 640px)';
+export const mediaToM = '(max-width: 639.9px)';
+export const mediaFromXl = '(min-width: 1440px)';
+export const mediaToXl = '(max-width : 1440px)';
+export const mediaFromXxl = '(min-width: 1920px)';
+export const mediaToXxl = '(max-width: 1920px)';
