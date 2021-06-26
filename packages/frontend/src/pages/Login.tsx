b74f558069227d69636d48581bf6fc1a87a01d5a
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-25 19:51:26 -0600

    login page added

---
+++ packages/frontend/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import RegisterForm from '../components/containers/RegisterForm';
+import TopMobileNav from '../components/containers/TopMobileNav';
+
+const Register = () => {
+  return (
+    <>
+      <TopMobileNav />
+      <RegisterForm />
+    </>
+  );
+};
+
+export default Register;
