0206e7a1f219cd2573c81c5bdc3f7ba507e4371c
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-06-27 19:10:42 -0600

    AsideDesktopNav component integrated

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -5,13 +5,20 @@ import ProductsHome from '../components/containers/ProductsHome';
 import ProductServices from '../services/ProductServices';
 import { IAllProducts, IProduct } from '../types/products';
 import BottomMobileNav from '../components/containers/BottomMobileNav';
+import { useMedia } from '../hooks/useMedia';
+import { mediaFrom1440 } from '../helpers/mediaQueries';
+import AsideDesktopNav from '../components/containers/AsideDesktopNav';
 
 const Page: FC<IHomeProps> = ({ drinks, guajolotas, tamales }) => {
+  const isDesktop = useMedia(mediaFrom1440);
   return (
     <>
       <TopMobileNav />
-      <ProductsHome drinks={drinks} guajolotas={guajolotas} tamales={tamales} />
-      <BottomMobileNav />
+      <main className="Index">
+        {isDesktop && <AsideDesktopNav />}
+        <ProductsHome drinks={drinks} guajolotas={guajolotas} tamales={tamales} />
+      </main>
+      {!isDesktop && <BottomMobileNav />}
     </>
   );
 };
