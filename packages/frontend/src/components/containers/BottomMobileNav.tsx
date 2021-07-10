89d738396dbfc6682ec7223c43ee69216200de0c
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-07-10 15:17:44 -0600

    Media queries addedc

--- packages/frontend/src/components/containers/BottomMobileNav.tsx
+++ packages/frontend/src/components/containers/BottomMobileNav.tsx
@@ -9,13 +9,13 @@ const BottomMobileNav = () => {
 
   return (
     <nav className="BottomMobileNav">
-      <div>
+      <div className="BottomMobileNav-asideDiv">
         <Image src="/images/history.svg" width={30} height={30} />
       </div>
       <div className="BottomMobileNav-profilePic">
         <ProfilePic profilePic={user.data.profilePic} isLogged={user.isLogged} />
       </div>
-      <div>
+      <div className="BottomMobileNav-asideDiv">
         <Image src="/images/graphs.svg" width={30} height={30} />
       </div>
     </nav>
