b0bf96273dcb6fd3da91f7e03558db6f0edf87f9
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-07-10 00:39:57 -0600

    NoProductSelected component styles added

--- packages/frontend/src/components/containers/AsideDesktopProduct.tsx
+++ packages/frontend/src/components/containers/AsideDesktopProduct.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image';
 import { useSelector } from 'react-redux';
 import { IProduct } from '../../types/products';
 import { IReduxState } from '../../types/redux';
+import NoProductSelected from '../molecules/NoProductSelected';
 
 const AsideDesktopProduct = () => {
   const productSelected = useSelector<IReduxState>((state) => state.productSelected) as IProduct;
 
   return (
     <aside>
-      {productSelected ? <ProductDetails /> : <NoProductSelectedCard />}
+      {productSelected ? <ProductDetails /> : <NoProductSelected />}
       <div>
         <Image src="/images/girl-with-cart.svg" layout="fill" />
       </div>
