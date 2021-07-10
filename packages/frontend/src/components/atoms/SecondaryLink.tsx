91cee2748cf38f0ec60e2a2f0d099f7b519ea323
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-07-09 23:47:04 -0600

    component SecondaryLink added

---
+++ packages/frontend/src/components/atoms/SecondaryLink.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { FC } from 'react';
+
+const SecondaryLink: FC<ISecondaryLinkProps> = ({ text, url }) => {
+  return (
+    <Link href={url}>
+      <a href="" className="SecondaryLink">
+        {text}
+      </a>
+    </Link>
+  );
+};
+
+interface ISecondaryLinkProps {
+  text: string;
+  url: string;
+}
+
+export default SecondaryLink;
