06190708dfb4b3d405c2bd7d67197cfa5c5efdf2
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-18 19:10:47 -0600

    change env variables

--- src/config/envVars.ts
+++ src/config/envVars.ts
@@ -1,5 +1,7 @@
-export const MODE: string = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
+export const MODE: string = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
 export const BASE_URL: string =
-  MODE === 'dev' ? process.env.NEXT_PUBLIC_BASE_API_URL_DEV : process.env.NEXT_PUBLIC_BASE_URL_PROD;
-export const API_KEY: string = MODE === 'dev' ? 'api_secret' : process.env.NEXT_PUBLIC_API_KEY;
+  MODE === 'dev'
+    ? process.env.NEXT_PUBLIC_BASE_API_URL_DEV
+    : process.env.NEXT_PUBLIC_BASE_API_URL_PROD
+export const API_KEY: string = MODE === 'dev' ? 'api_secret' : process.env.NEXT_PUBLIC_API_KEY
 export const PORT = Number(process.env.NEXT_PUBLIC_API_PORT)
