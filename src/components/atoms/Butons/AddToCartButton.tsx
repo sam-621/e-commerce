c319a055708eb3ffa258c3c345d683d1d15d1c0f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-18 21:01:07 -0600

    now you cant add products repeated

--- src/components/atoms/Butons/AddToCartButton.tsx
+++ src/components/atoms/Butons/AddToCartButton.tsx
@@ -1,26 +1,30 @@
-import { addProductToCartWithNoSession } from '@Helpers/cart';
-import { IProduct } from '@Types/products';
-import { IAction } from '@Types/redux';
-import { FC } from 'react';
-import { useDispatch } from 'react-redux';
-import { Dispatch } from 'redux';
+import { addProductToCartWithNoSession } from '@Helpers/cart'
+import { IProduct } from '@Types/products'
+import { IAction, IReduxState } from '@Types/redux'
+import { FC } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Dispatch } from 'redux'
 
 const AddToCartButton: FC<IAddToCartButtonProps> = ({ product }) => {
-  const dispatch = useDispatch<Dispatch<IAction>>();
+  const dispatch = useDispatch<Dispatch<IAction>>()
+  const cart = useSelector<IReduxState>((state) => state.cart) as IProduct[]
+  const isInTheCart = cart.some((item) => item.id === product.id)
 
   return (
-    <button
-      className="AddToCartButton"
-      type="button"
-      onClick={() => addProductToCartWithNoSession(product, dispatch)}
-    >
-      <img src="/images/add.svg" width={20} height={20} />
-    </button>
-  );
-};
-
-interface IAddToCartButtonProps {
-  product: IProduct;
+    !isInTheCart && (
+      <button
+        className='AddToCartButton'
+        type='button'
+        onClick={() => addProductToCartWithNoSession(product, dispatch)}
+      >
+        <img src='/images/add.svg' width={20} height={20} />
+      </button>
+    )
+  )
 }
 
-export default AddToCartButton;
+interface IAddToCartButtonProps {
+  product: IProduct
+}
+
+export default AddToCartButton
