e9993c4fcb2ac1548c526cefdf7c4cc4d41373c2
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-26 11:39:43 -0600

    Toast component created

---
+++ src/components/atoms/Toast.tsx
@@ -0,0 +1,19 @@
+import { ToastContainer } from 'react-toastify'
+
+const Toast = () => {
+  return (
+    <ToastContainer
+      position="bottom-left"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+  )
+}
+
+export default Toast
