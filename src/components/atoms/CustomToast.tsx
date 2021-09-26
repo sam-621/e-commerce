c2e15d8479fdeded8c8147d0b58dfa756c9ff22c
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-26 11:39:43 -0600

    Toast component created

---
+++ src/components/atoms/CustomToast.tsx
@@ -0,0 +1,19 @@
+import { ToastContainer } from 'react-toastify'
+
+const CustomToast = () => {
+  return (
+    <ToastContainer
+      position="bottom-left"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+  )
+}
+
+export default CustomToast
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,13 +1,15 @@
-import RegisterForm from '@Components/containers/RegisterForm';
-import TopMobileNav from '@Components/containers/Mobile/TopMobileNav';
+import RegisterForm from '@Components/containers/RegisterForm'
+import TopMobileNav from '@Components/containers/Mobile/TopMobileNav'
+import CustomToast from '@Components/atoms/CustomToast'
 
 const Register = () => {
   return (
     <>
       <TopMobileNav />
       <RegisterForm />
+      <CustomToast />
     </>
-  );
-};
+  )
+}
 
-export default Register;
+export default Register
